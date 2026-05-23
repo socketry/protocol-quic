23f8bf4b6bbd6e7ad5a1f3c47f63fd572e7eafa6
Samuel Williams <samuel.williams@shopify.com>
2026-05-23 20:24:47 +0900

    Fix extension rakefile

--- ext/rakefile.rb
+++ ext/rakefile.rb
@@ -1,12 +1,9 @@
+# frozen_string_literal: true
 
 task :default do
-	ruby_root = ENV['RUBY_ROOT']
-	install_prefix = ENV['RUBYARCHDIR']
+	ruby_library_directory = ENV.fetch("RUBYLIBDIR"){ENV.fetch("RUBYARCHDIR")}
+	build_environment = {"RUBYLIBDIR" => ruby_library_directory}
 	
-	"RUBYARCHDIR"=>"/home/samuel/.gem/ruby/3.2.1/extensions/x86_64-linux/3.2.0-static/protocol-quic-0.0.0",
-	"RUBYLIBDIR"=>"/home/samuel/.gem/ruby/3.2.1/extensions/x86_64-linux/3.2.0-static/protocol-quic-0.0.0"}
- 
-	
-	$stderr.puts ENV.inspect
-	exit!(1)
+	sh build_environment, "teapot", "fetch"
+	sh build_environment, "teapot", "Ruby/Protocol/QUIC"
 end

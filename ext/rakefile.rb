05b9bac366d0cb983c7eee14a21ad465ca4da19b
Samuel Williams <samuel.williams@shopify.com>
2026-05-25 10:06:09 +0900

    Resolve Teapot executable during extension build.

--- ext/rakefile.rb
+++ ext/rakefile.rb
@@ -3,10 +3,14 @@
 # Released under the MIT License.
 # Copyright, 2023-2026, by Samuel Williams.
 
+require "rbconfig"
+require "rubygems"
+
 task :default do
 	ruby_library_directory = ENV.fetch("RUBYLIBDIR"){ENV.fetch("RUBYARCHDIR")}
 	build_environment = {"RUBYLIBDIR" => ruby_library_directory}
+	teapot = [RbConfig.ruby, Gem.bin_path("teapot", "teapot")]
 	
-	sh build_environment, "teapot", "fetch"
-	sh build_environment, "teapot", "Ruby/Protocol/QUIC"
+	sh build_environment, *teapot, "fetch"
+	sh build_environment, *teapot, "Ruby/Protocol/QUIC"
 end
